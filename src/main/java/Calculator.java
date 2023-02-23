c0009ca2b050317add7cc1db0513d902cc8ae95e
Michal Gawlas <gawlas05@gmail.com>
2023-02-23 17:50:56 +0100

    adding olny

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,16 +3,4 @@ public class Calculator {
     public static int addingAtoB(int a, int b) {
         return a+b;
     }
-
-    public static int subtractingAfromB(int a, int b) {
-        return b-a;
-    }
-
-    public static int multiplying(int a, int b) {
-        return a*b;
-    }
-
-    public static double dividing (double a, double b) {
-        return a/b;
-    }
 }
